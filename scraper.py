5b5ed686fe96d69829922dde8d4e92bf20c181eb
Patrick Brady <patrick.brady@gmail.com>
2021-10-11 12:25:24 +0000

    WIP - add todo for new jersey journal system

--- scraper.py
+++ scraper.py
@@ -6,8 +6,15 @@ import pickle
 from itertools import chain 
 
 def getJerseyJournalListings():
+    """
+    As of 10/10/2021 the classifieds section has been revised to a new system that uses 
+    images that may or may not have transcriptions inside of a show more link
+    to-do: walk through results for weehawken keyword search, collect post dates and show more links.
+    Visit each show more link and either collect the transcript or collect and OCR the image?
+    """
     jjDomain = 'http://classifieds.nj.com'
-    jjPath = '/?tp=ME_nj&cur_cat=6869&category=results&property=nj.com&temp_type=browse&ads_per_page=100'
+    jjPath = '/marketplace/nj/search/query?categoryId=2&searchProfile=general&source=nj&page=1&size=100&view=list&showExtended=false&startRange=&keywords=weehawken&location=&postCode=&searchRadius=&searchUnit=MILES&customField1=&customField2=&customField3=&customField4=&customField5=&customField6=&customField7=&customField8=&customField9=&customField10=&ordering='
+    # jjPath = '/?tp=ME_nj&cur_cat=6869&category=results&property=nj.com&temp_type=browse&ads_per_page=100'
     listings = getWeehawkenListingsForSite(jjDomain, jjPath)
     return listings
 
